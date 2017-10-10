0c76fe9052deb62f83b1f627e4210246c1c9f983
Frankchesco Leveratto <leverattoc@outlook.com>
2017-10-10 23:05:32 +0100

    Added Point class

---
+++ Point.h
@@ -0,0 +1,19 @@
+#ifndef included_PointClass
+#define included_PointClass
+
+class Point {
+  //CLASS INVARIENT (CI): this object holds two integers the
+  //represent the x and y coordinate on a 2 dimensional
+  //space.
+ private:
+  int x_coor;
+  int y_coor;
+
+ public: 
+  //PRE: x and y are two positive integers
+  //POST: CI satisfied.
+  Point(int x, int y);
+  
+}
+
+#endif
